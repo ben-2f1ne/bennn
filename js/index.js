3c297b7e7556128a8bcee72f0b3255ff013540ec
ben-2f1ne <benjielibunao321@gmail.com>
2025-02-10 17:36:30 +0800

    hehe

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'HAPPY VALENTINES BOSS'.split('')
+const text = 'HAPPY VALENTINES ANGE!!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
